60f8c5aee0eec61440c4506059dc7c1bc276b8d1
Joshua Rubin <me@jawa.dev>
2020-04-30 11:45:20 -0600

    add adafruit bluefruit le uart support
    Signed-off-by: Joshua Rubin <me@jawa.dev>

--- keyboards/bioi/g60ble/rules.mk
+++ keyboards/bioi/g60ble/rules.mk
@@ -16,7 +16,7 @@ BOOTLOADER = qmk-dfu
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = lite    # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes      # Mouse keys(+4700)
+# MOUSEKEY_ENABLE = yes    # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes      # Audio control and System control(+450)
 # CONSOLE_ENABLE = yes     # Console for debug(+400)
 COMMAND_ENABLE = yes       # Commands for debug and configuration
@@ -25,5 +25,6 @@ NKRO_ENABLE = yes          # USB Nkey Rollover - if this doesn't work, see here:
 BACKLIGHT_ENABLE = yes     # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes
 LTO_ENABLE = yes
+BLUETOOTH = AdafruitBLEUART
 
 LAYOUTS = 60_ansi 60_iso 60_hhkb 60_ansi_split_bs_rshift 60_tsangan_hhkb
